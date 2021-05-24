1a2413c03de36cfa9d32a5d0f8a29408f2c6d1ef
Adam Fowler <adamfowler71@gmail.com>
2021-05-24 11:00:05 +0100

    Add precondition, to ensure we aren't running with pipelining assist

--- Sources/HummingbirdCompression/Application+Compression.swift
+++ Sources/HummingbirdCompression/Application+Compression.swift
@@ -18,11 +18,13 @@ extension HBApplication {
     /// Add Channel Handler for decompressing request that have Content-Encoding header set to gzip or deflate
     /// - Parameter limit: Indicate the memory limit of how much to decompress to
     public func addRequestDecompression(limit: HTTPDecompressionLimit) {
+        precondition(self.configuration.enableHttpPipelining, "Request decompression does not work without HTTP pipelining assist enabled")
         self.server.addRequestDecompression(limit: limit, threadPool: self.threadPool)
     }
 
     /// Add Channel Handler for compressing responses where accept-encoding header indicates the client will accept compressed data
     public func addResponseCompression() {
+        precondition(self.configuration.enableHttpPipelining, "Response compression does not work without HTTP pipelining assist enabled")
         self.server.addResponseCompression(threadPool: self.threadPool)
     }
 }
