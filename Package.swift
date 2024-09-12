a5656930b42edbd25ecd355aa87fe6e05ca97857
Adam Fowler <adamfowler71@gmail.com>
2024-09-12 09:40:42 +0100

    Hummingbird 2.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "HummingbirdCompression", targets: ["HummingbirdCompression"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-rc.4"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
         .package(url: "https://github.com/adam-fowler/compress-nio.git", from: "1.2.1"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.32.1"),
     ],
